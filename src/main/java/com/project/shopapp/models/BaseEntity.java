33e249b0a0b162a00d18f9d1db3d58aa4cdca597
lmthongit98 <75925571+lmthongit98@users.noreply.github.com>
2024-01-13 01:28:50 +0700

    config jpa auditing

--- src/main/java/com/project/shopapp/models/BaseEntity.java
+++ src/main/java/com/project/shopapp/models/BaseEntity.java
@@ -9,6 +9,7 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 
 import java.time.LocalDateTime;
@@ -17,7 +18,7 @@ import java.time.LocalDateTime;
 @Getter
 @NoArgsConstructor
 @MappedSuperclass
-//@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -34,11 +35,11 @@ public class BaseEntity {
 
     @LastModifiedDate
     @Column(name = Base.UPDATED_AT)
-    protected LocalDateTime lastModifiedAt;
+    protected LocalDateTime updatedAt;
 
     @LastModifiedBy
     @Column(name = Base.UPDATED_BY)
-    protected String lastModifiedBy;
+    protected String updatedBy;
 }
 
 class Base {
